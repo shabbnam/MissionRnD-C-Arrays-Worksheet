4c937711c32e7b35f2c611c21b4a494ba2fde20e
shabbnam <shabrnd@gmail.com>
2015-11-11 18:51:51 +0530

    Submit Commit

--- src/removeArrayDuplicates.cpp
+++ src/removeArrayDuplicates.cpp
@@ -12,10 +12,50 @@ NOTES: Don't create new array, try to change the input array.
 */
 
 #include <stdio.h>
+#include<stdlib.h>
+
+
+int search(int Arr[], int num, int j)
+{
+	int i;
+	for (i = 0; i <= j; i++)
+	{
+		if (Arr[i] == num)
+			return 1;
+	}
+
+	return 0;
+
+}
+
+void  remove_duplicates(int Arr[], int len)
+{
+	int i, j = 0;
+	for (i = 1; i<len;)
+	{
+		if (search(Arr, Arr[i], j))
+		{
+		}
+		else{ Arr[++j] = Arr[i];  }
+		i++;
+
+	}
+	
+
+}
+
+
+
 
 void * removeArrayDuplicates(int *Arr, int len)
-
 {
+
+	if (Arr == NULL || len <= 0)return NULL;
+	else
+	{
+		 remove_duplicates(Arr, len);
+
+	}
 	
 	return NULL;
 }
